095aa6563048282cdc030499083c82f6161af4af
Mokhlesur Mahin <mokhlesurr031@gmail.com>
2024-03-24 00:42:34 +0600

    write: unit test

--- db/sqlc/entries.sql.go
+++ db/sqlc/entries.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.25.0
 // source: entries.sql
 
-package sqlc
+package db
 
 import (
 	"context"
--- db/sqlc/transfer.sql.go
+++ db/sqlc/transfer.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.25.0
 // source: transfer.sql
 
-package sqlc
+package db
 
 import (
 	"context"
